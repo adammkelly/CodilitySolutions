ac6ebd947ea0b4e9de564354174a4fe81acf892c
Adam Kelly <adammkelly@live.com>
2017-06-24 16:08:40 +0100

    PermMissingElem - Remove printout

--- 3.TimeComplexity/PermMissingElem.c
+++ 3.TimeComplexity/PermMissingElem.c
@@ -1,3 +1,4 @@
+<<<<<<< current
 #include <stdio.h>
 
 #define SIZE 4
@@ -49,3 +50,37 @@ int main(int argc, char const *argv[]) {
   printf("Missing Elem: %d\n", solution(A, SIZE));
   return 0;
 }
+=======
+#include <stdio.h>
+
+#define SIZE 4
+
+int A[SIZE] = {4, 3, 1, 5};
+
+int solution(int A[], int N) {
+     if(N == 0) {
+        return 1;
+    }
+    if (N == 1) {
+        if (A[0] != 1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+    int sum = 0;
+    int idxSum = 0;
+    for(int i = 0;i < N ; i ++) {
+        idxSum += i +1;
+        sum += A[i];
+    }
+
+    return idxSum + N + 1 - sum;
+}
+
+int main(int argc, char const *argv[]) {
+
+  printf("Missing Elem: %d\n", solution(A, SIZE));
+  return 0;
+}
+>>>>>>> before discard
