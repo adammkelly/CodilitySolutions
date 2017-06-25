1c189fc165113494bc223eaf7b0188cb327f35e2
Adam Kelly <adammkelly@live.com>
2017-06-25 11:35:36 +0100

    Distinct - 100% Correct and Performance

--- 6.Sorting/Distinct.c
+++ 6.Sorting/Distinct.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SIZE 6
-int A[SIZE] = {2, 1, 1, 2, 3, 1};
+int A[SIZE] = {-1, -2, -1, 2, 3, 1};
 
 // TODO: REDO, 33% Correct, 0 Perf WIP
+
+int compare(const void *a, const void *b)
+{
+  return ( *(int*)a - *(int*)b );
+}
 
 int solution(int A[], int N)
 {
   int curr = 0;
-  int *B;
+  int distinct = 0;
+  int last;
 
-  B = (int *)calloc(N, sizeof (int));
+  if (N == 1) {
+    return 1;
+  }
+
+  qsort(A, N, sizeof(int), compare);
 
   for (int i = 0; i < N; i++) {
-    curr = A[i] < 0 ? abs(A[i]) : A[i];
-    if (A[i] < 0 && !(B[curr - 1] & 0x1)) {
-        B[curr - 1] |= 0x1;
-    } else if (A[i] > 0 && !(B[curr - 1] & 0x2)) {
-        B[curr - 1] |= 0x2;
+    curr = A[i];
+    if (curr != last) {
+      distinct++;
     }
+    last = curr;
   }
 
-  curr = 0;
-  for (int i = 0; i < N; i++) {
-      if ((B[i] & 0x3) == 0x3) {
-        curr += 2;
-      } else if ((B[i] & 0x1) == 0x1) {
-        curr++;
-      } else if ((B[i] & 0x2) == 0x2) {
-        curr++;
-      }
-      B[i] = 0;
-  }
 
-  return curr;
+  return distinct;
+}
+
+int main(int argc, char const *argv[]) {
+  printf("Distinct: %d", solution(A, SIZE));
+  return 0;
 }
